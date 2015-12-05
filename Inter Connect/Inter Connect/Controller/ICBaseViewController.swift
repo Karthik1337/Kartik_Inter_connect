7e59b6cd5883a183406d84ba557dab556b7109f5
karthik <jin.karthik@gmail.com>
2015-12-05 11:59:25 +0530

    Project Settings Updated.

---
+++ Inter Connect/Inter Connect/Controller/ICBaseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ICBaseViewController.swift
+//  Inter Connect
+//
+//  Created by KARTHIK on 05/12/15.
+//
+//
+
+import UIKit
+
+class ICBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
